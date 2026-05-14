e4bcea26beadea8926f186e9adf171b895b8328b
MuthukrishnanU <muthuk60@gmail.com>
2026-05-14 11:08:56 +0530

    removing hardcoded keys

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,11 +5,15 @@ import os
 load_dotenv()
 
 class Settings(BaseSettings):
-    MONGODB_URI: str = os.getenv("MONGODB_URI", "mongodb+srv://dbUser:dbPwd123@cluster0.l5s8qju.mongodb.net/?appName=Cluster0&tlsAllowInvalidCertificates=true")
-    DB_NAME: str = os.getenv("DB_NAME", "bankingPolicyDB")
-    COLLECTION_FILES: str = os.getenv("COLLECTION_FILES", "bankingPolicyDocs")
-    COLLECTION_METRICS: str = os.getenv("COLLECTION_METRICS", "bankingPolicyMetricsValues")
-    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY", "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")
-    VECTOR_INDEX_NAME: str = os.getenv("VECTOR_INDEX_NAME", "bankingPolicyVectorDB")
+    MONGODB_URI: str = os.getenv("MONGODB_URI")
+    DB_NAME: str = os.getenv("DB_NAME")
+    COLLECTION_FILES: str = os.getenv("COLLECTION_FILES")
+    COLLECTION_METRICS: str = os.getenv("COLLECTION_METRICS")
+    COLLECTION_USERS: str = os.getenv("COLLECTION_USERS")
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+    VECTOR_INDEX_NAME: str = os.getenv("VECTOR_INDEX_NAME")
+    GOOGLE_API_KEY: str = os.getenv("GOOGLE_API_KEY")
+    MISTRALAI_API_KEY: str = os.getenv("MISTRALAI_API_KEY")
+    HUGGINGFACEHUB_API_TOKEN: str = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
 settings = Settings()
